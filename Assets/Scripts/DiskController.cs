b05c557d55a53038e2c02d89c978d51e68bdb5b3
daxiongmoa87 <daxiongmao87@gmail.com>
2018-02-07 16:21:17 -0600

    Finished basic player control, implemented basic disk control

---
+++ Assets/Scripts/DiskController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiskController : MonoBehaviour {
+  public float diskSpeed = 10;
+  bool diskFired = false;
+  // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(diskFired) {
+      transform.Translate(Vector3.forward * diskSpeed * Time.deltaTime);
+		}
+	}
+	
+	public bool GetFired() {
+    return diskFired;
+	}
+	
+	public void Fire(GameObject player, GameObject playerCamera) {
+    transform.rotation = playerCamera.transform.rotation;
+    diskFired = true;
+    gameObject.transform.parent = null;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+Player Controller handles all player controls, to include movement and disk manipulation
+*/
+public class PlayerController : MonoBehaviour {
+  //Camera Object for Looking
+  public GameObject playerCamera;
+  //Disk object and prefab
+  public GameObject prefDisk;
+  GameObject objDisk;
+  public DiskController diskController;
+  // Player Speed Scalar Variable
+  public float playerSpeed = 1f;
+  // Player Rotation Speed Scalar Variable
+  public float playerTurnSpeed = 10f;
+  
+ 
+  void Start () {
+		
+	}
+	
+	void Update () {
+    //Shortened variable name for WASD for convenience 
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+		//Logic to translate input to ingame movement
+		Vector3 playerPosition = gameObject.transform.position;
+    gameObject.transform.Translate(new Vector3 (horizontalInput * Time.deltaTime * playerSpeed, 0, verticalInput * Time.deltaTime * playerSpeed));
+		gameObject.transform.Rotate(Vector3.up * Input.GetAxis("Mouse X"));
+		//Player Camera Look
+		playerCamera.transform.Rotate(Vector3.right * -Input.GetAxis("Mouse Y"));
+		//Logic to translate input to shoot disks
+		if(Input.GetAxis("Fire1") == 1 && !diskController.GetFired()) {
+      diskController.Fire(gameObject, playerCamera);
+		}
+		else if(Input.GetAxis("Fire1") == 0 && diskController.GetFired()) {
+		}
+		
+	}
+}
