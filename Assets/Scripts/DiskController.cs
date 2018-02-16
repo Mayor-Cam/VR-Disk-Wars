6985d1fe4bea1d948a0c71c9f1488211e94062d3
daxiongmao87 <daxiongmao87@gmail.com>
2018-02-15 20:48:26 -0600

    Completed BASIC disk physics.  Not perfect, but fully functional.  I still need to properly calculate disk presence when 'colliding' against a wall

--- Assets/Scripts/DiskController.cs
+++ Assets/Scripts/DiskController.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class DiskController : MonoBehaviour {
   public float diskSpeed = 10;
   bool diskFired = false;
+  SphereCollider collider;
   // Use this for initialization
 	void Start () {
-	
+    collider = GetComponent<SphereCollider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(diskFired) {
-      transform.Translate(Vector3.forward * diskSpeed * Time.deltaTime);
+      Vector3 orig = transform.position;
+      float rad = collider.radius;
+      RaycastHit hit;
+      
+      if(Physics.SphereCast(orig,rad,transform.forward, out hit, diskSpeed* Time.deltaTime)) {
+        Debug.Log("B: " + transform.forward);
+        Vector3 reflect = Vector3.Reflect(transform.forward, hit.normal);
+        Debug.Log("A: " + reflect);
+        transform.forward = reflect;
+        transform.position = hit.point+ transform.forward*0.5f*transform.localScale.x + (reflect*((diskSpeed*Time.deltaTime)-hit.distance)); 
+        
+      } 
+      else {
+        transform.Translate(Vector3.forward * diskSpeed * Time.deltaTime);
+        }
+      orig = transform.position;
+      Debug.DrawRay(orig+transform.forward*0.5f*transform.localScale.x,transform.forward*diskSpeed*Time.deltaTime,Color.red);
 		}
 	}
 	
@@ -26,4 +43,6 @@ public class DiskController : MonoBehaviour {
     diskFired = true;
     gameObject.transform.parent = null;
 	}
+	
+
 }
