9dade7c01c3b6531b22e41e62451c748b57433bf
daxiongmao87 <daxiongmao87@gmail.com>
2018-02-22 18:56:31 -0600

    using SweepTest for collision detection.  However, the current implementation only tests for one collision per frame.  It is possible that on corners, two or three collisions could happen, and since this isn't accounted for, sometimes the disk will leave the play area.  The solution is to do a while loop through the distance of the disk travelling in one frame for more sweep tests

--- Assets/Scripts/DiskController.cs
+++ Assets/Scripts/DiskController.cs
@@ -6,28 +6,31 @@ public class DiskController : MonoBehaviour {
   public float diskSpeed = 10;
   bool diskFired = false;
   SphereCollider collider;
+  Rigidbody rb;
   // Use this for initialization
 	void Start () {
     collider = GetComponent<SphereCollider>();
+    rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		if(diskFired) {
       Vector3 orig = transform.position;
-      float rad = collider.radius;
+//      float rad = collider.radius;
       RaycastHit hit;
       
-      if(Physics.SphereCast(orig,rad,transform.forward, out hit, diskSpeed* Time.deltaTime)) {
+      //if(Physics.SphereCast(orig,rad,transform.forward, out hit, diskSpeed* Time.deltaTime)) {
+      if(rb.SweepTest(transform.forward, out hit, diskSpeed*Time.fixedDeltaTime)) {        
         Debug.Log("B: " + transform.forward);
         Vector3 reflect = Vector3.Reflect(transform.forward, hit.normal);
         Debug.Log("A: " + reflect);
         transform.forward = reflect;
-        transform.position = hit.point+ transform.forward*0.5f*transform.localScale.x + (reflect*((diskSpeed*Time.deltaTime)-hit.distance)); 
+        transform.position = hit.point+ transform.forward*0.5f*transform.localScale.x + (reflect*((diskSpeed*Time.fixedDeltaTime)-hit.distance)); 
         
       } 
       else {
-        transform.Translate(Vector3.forward * diskSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * diskSpeed * Time.fixedDeltaTime);
         }
       orig = transform.position;
       Debug.DrawRay(orig+transform.forward*0.5f*transform.localScale.x,transform.forward*diskSpeed*Time.deltaTime,Color.red);
